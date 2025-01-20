5ef86c5378c0115b00fd0177f24b96a600aa2617
gitDemo <nitinbone5225@gmail.com>
2025-01-20 12:39:05 +0530

    Initial commit of backend project

---
+++ src/main/java/com/backend_to_do/BackendToDoApplication.java
@@ -0,0 +1,14 @@
+package com.backend_to_do;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendToDoApplication.class, args);
+		System.out.println("hii");
+	}
+
+}
